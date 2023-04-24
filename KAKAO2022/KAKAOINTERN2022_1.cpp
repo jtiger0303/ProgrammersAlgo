ca1a48a9a29967e412ee8f11d5293c8e816d3fa8
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-04-25 04:15:58 +0900

    Initial Commit for 카카오 성격 유형 검사하기

---
+++ KAKAO2022/KAKAOINTERN2022_1.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+using namespace std;
+map<char, int> score;
+string solution(vector<string> survey, vector<int> choices) {
+    string answer = "";
+    int n = survey.size();
+
+    for (int i = 0; i < n; i++) {
+        if (choices[i] <= 3) {
+            //매우 비동의, 비동의, 약간 비동의일 경우
+            score[survey[i][0]] += 4 - choices[i];
+        }
+        else score[survey[i][1]] += choices[i] - 4;
+    }
+
+    if (score['R'] >= score['T']) answer += 'R';
+    else answer += 'T';
+
+    if (score['C'] >= score['F']) answer += 'C';
+    else answer += 'F';
+
+    if (score['J'] >= score['M']) answer += 'J';
+    else answer += 'M';
+
+    if (score['A'] >= score['N']) answer += 'A';
+    else answer += 'N';
+
+    return answer;
+}
