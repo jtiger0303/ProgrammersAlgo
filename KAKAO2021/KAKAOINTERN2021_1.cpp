acad9e18798d8a8f43a0f581c8ac1d9899d0e313
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-07-25 04:18:56 +0900

    Initial Commit for 숫자 문자열과 영단어 using replace

---
+++ KAKAO2021/KAKAOINTERN2021_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int solution(string s) {
+    string arr[10] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+    for (int i = 0; i < 10; i++) {
+        while (true) {
+            auto pos = s.find(arr[i]);
+            if (pos == string::npos) break;
+            s.replace(pos, arr[i].size(), to_string(i));
+        }
+    }
+    return stoi(s);
+}
